5f210c33a0cc63fdeea8038ee5978031a5b4ea5f
dinxsh <16219421127@atmiyauni.edu.in>
2023-07-05 18:34:30 +0530

    ayo

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const path = require('path')
+var bodyParser = require('body-parser');
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+const app = express()
+
+app.use('/', express.static(path.join(__dirname, '../frontend/')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/index.html'));
+})
+
+app.post('/login', urlencodedParser, (req, res) => {
+    res.send({ username: req.body.email, password: req.body.password })
+});
+
+app.listen(3000, () => { console.log('port is running'); })
